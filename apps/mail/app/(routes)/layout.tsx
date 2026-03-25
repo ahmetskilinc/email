9274b126f29bf0afceaa4127b8fb39ebc8d37414
Ahmet Kilinc <akx9@icloud.com>
2026-03-25 01:10:40 +0000

    fix layout

--- apps/mail/app/(routes)/layout.tsx
+++ apps/mail/app/(routes)/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <AppSidebar />
-      <SidebarInset className="h-svh md:h-[calc(100vh-1rem)]">
+      <SidebarInset className="h-dvh md:h-[calc(100dvh-1rem)]">
         <SiteHeader />
         <ConnectionSyncer />
         <div className="relative flex h-full w-full flex-1 overflow-hidden">{children}</div>
